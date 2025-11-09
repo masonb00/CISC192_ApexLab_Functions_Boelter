a948c38ea6aaacadf4fe6e396bfbd816ae434ab4
masonb00 <85399414+masonb00@users.noreply.github.com>
2025-11-08 17:29:50 -0800

    Test for git commit

--- lab_code/apex_code.cpp
+++ lab_code/apex_code.cpp
@@ -4,3 +4,27 @@
 #include "apex_code.h"
 
 using namespace std;
+
+int getNumberTiles(double heightInFt, double widthInFt, int tileSizeInIn)
+{
+    //Convert input dimensions into inches
+    double heightInIn = heightInFt * 12.0;
+    double widthInIn = widthInFt * 12.0;
+
+    //Calculate number of tiles needed along height
+    int tilesAlongHeight = static_cast<int>((heightInIn / tileSizeInIn) + 0.9999);
+
+    //Calculate number of tiles needed along width
+    int tilesAlongWidth = static_cast<int>((widthInIn / tileSizeInIn) + 0.9999);
+
+    //Calculate total number of tiles needed
+    int numTilesNeeded = tilesAlongHeight * tilesAlongWidth;
+
+    return numTilesNeeded;
+}
+
+bool upperCharAnd (char char1, char char2)
+{
+    //return true only if both characters are uppercase letters
+    return (isupper(char1) && isupper(char2));
+}
